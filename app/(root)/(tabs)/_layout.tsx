b5f6f44bc9cc8267f9b9884c89bf188167f042bc
Zeeshan <xeshanmalik107@gmail.com>
2025-03-30 18:26:55 +0500

    Tab bar menu

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,11 +1,84 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { FontAwesome6, Ionicons } from '@expo/vector-icons'
+import { View } from 'react-native'
 
 const _layout = () => {
+
+
+
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+   <Tabs 
+   initialRouteName='index'
+   screenOptions={{
+    headerShown:false,
+    tabBarActiveTintColor:"#FF0052",
+    tabBarStyle:{
+    // position:"absolute",
+    bottom:20,
+    // left: 20,
+    // right: 20,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor: "white",
+    borderRadius: 50,
+    height: 55,
+    elevation: 5,
+    }
+   }}>
+    <Tabs.Screen name="index" 
+    options={{ 
+        headerShown: false,
+        animation:"shift",
+        title:"",
+        tabBarIcon:({focused})=>{
+            return (
+                <View className={`flex flex-row items-center justify-center `}>
+                    <FontAwesome6 name="house" size={26} color={`${focused? "#FF0052":"#CCCCCC"}`} />
+                </View>
+            )
+        },
+        tabBarItemStyle:{            
+            justifyContent:"center",
+            alignItems:"center",
+            elevation: 5,
+        }
+     }} />
+
+    <Tabs.Screen name="donate" 
+    options={{
+        title:"",
+        headerShown:false,
+        tabBarIcon:({focused})=> {
+            return(
+                <View
+                className='bg-primary rounded-full h-24 w-24 items-center justify-center '>
+                    <Ionicons name="water-outline" size={30} color="white" />
+                </View>
+            )
+        },
+        animation:"shift",
+        tabBarItemStyle:{
+            justifyContent:"center",
+            alignItems:"center",
+            elevation: 5,
+        }
+        }}/>
+
+    <Tabs.Screen name="profile" 
+    options={{
+        headerShown:false,
+        animation:"shift",
+        title:"",
+        tabBarIcon:({focused})=>{
+            return (
+                <View className={`flex flex-row items-center justify-center `}>
+                    <Ionicons name="person" size={26} color={`${focused? "#FF0052":"#CCCCCC"}`} />
+                </View>
+            )
+        }
+        }}/>
+   </Tabs>
   )
 }
 
