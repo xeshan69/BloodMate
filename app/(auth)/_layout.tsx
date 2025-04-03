27640da84d5c415d472dfb91ea59b063fd24f4da
Zeeshan <xeshanmalik107@gmail.com>
2025-04-03 14:15:37 +0500

    Rename initial screen to 'onboarding1' in authentication layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,10 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
 
 const _layout = () => {
   return (
     <Stack>
-        <Stack.Screen name='index' options={{headerShown:false}}/> 
+        <Stack.Screen name='onboarding1' options={{headerShown:false}}/> 
         <Stack.Screen name='onboarding2' options={{headerShown:false}}/> 
     </Stack>
   )
